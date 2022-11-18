ecd2bfe4c4baf6f90bf140708dba10a45bccd751
stefanIvasca <stefanivasca@gmail.com>
2022-11-18 12:01:32 +0200

    write the method and take input from user

---
+++ code.rb
@@ -0,0 +1,15 @@
+#write a method with two argumens, a string and a number
+#take input from user and pass it to a variable
+
+#take this variable and iterate trowgth it
+#for anything alse that letters, keep it, change the letters
+#take ascii code for every letter and add number to it
+#if this operation is equal with the equivalent of z, go to equivalent of a
+#return the new string 
+
+def caesar_cipher (forward_shift_number)
+    user_input = gets
+    puts forward_shift_number
+end
+
+caesar_cipher(3)
