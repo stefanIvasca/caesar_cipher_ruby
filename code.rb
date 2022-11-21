8cf0dd773c09b77c78154618cea376c7f8ac4e78
stefanIvasca <stefanivasca@gmail.com>
2022-11-21 10:55:10 +0200

    insert cases, buid logic and display result

--- code.rb
+++ code.rb
@@ -1,15 +1,18 @@
-#write a method with two argumens, a string and a number
-#take input from user and pass it to a variable
-
-#take this variable and iterate trowgth it
-#for anything alse that letters, keep it, change the letters
-#take ascii code for every letter and add number to it
-#if this operation is equal with the equivalent of z, go to equivalent of a
-#return the new string 
-
 def caesar_cipher (forward_shift_number)
     user_input = gets
-    puts forward_shift_number
+    result = ''
+    user_input.each_char { |carracter| 
+        shifted_letter = carracter.ord + forward_shift_number
+        if shifted_letter > 122 && (carracter.ord <= 122 && carracter.ord >= 96)
+            shifted_letter = shifted_letter - 122 + 96
+        elsif shifted_letter > 90 && (carracter.ord <= 90 && carracter.ord >= 65)
+            shifted_letter = shifted_letter - 90 + 64  
+        elsif carracter.ord < 65 || (carracter.ord > 90 && carracter.ord < 97) || carracter.ord > 122
+            shifted_letter = carracter
+        end
+        result += shifted_letter.chr
+    }
+    puts result
 end
 
 caesar_cipher(3)
